d3301546de7c4d22ab7d8cd5b8b42323f1b58507
Aleksa <axela.01@gmail.com>
2016-03-16 12:26:03 +0100

    first changes- modified some files

--- JavnaNabavkaUpp/src/main/java/activiti/spring/javnaNabavka/springweb/persistence/services/RadSaPrijavamaService.java
+++ JavnaNabavkaUpp/src/main/java/activiti/spring/javnaNabavka/springweb/persistence/services/RadSaPrijavamaService.java
@@ -1,5 +1,7 @@
 package activiti.spring.javnaNabavka.springweb.persistence.services;
 
+import java.util.ArrayList;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -7,17 +9,23 @@ import org.springframework.stereotype.Service;
 
 import activiti.spring.javnaNabavka.enitity.Prijava;
 @Service("SacuvajPrijavuService")
-public class SacuvajPrijavuService {
+public class RadSaPrijavamaService {
 
 	@PersistenceContext
 	private EntityManager entityManager;
 	
 	public Prijava sacuvaj(String nazivPodnosiocaPrijave, String adresaPodnosiocaPrijave,String emailPodnosiocaPrijave){
 		
-		Prijava prijava = new Prijava(nazivPodnosiocaPrijave,adresaPodnosiocaPrijave,emailPodnosiocaPrijave);
-		
+	Prijava prijava = new Prijava(nazivPodnosiocaPrijave,adresaPodnosiocaPrijave,emailPodnosiocaPrijave);
+	
 		entityManager.persist(prijava);
 		return prijava;
 		
+	}
+	
+	@SuppressWarnings("unchecked")
+	public ArrayList<Prijava> listaPrijavljenih(){
+		System.out.println( entityManager.createNamedQuery("SELECT * FROM Prijava"));
+		return (ArrayList<Prijava>) entityManager.createNamedQuery("SELECT * FROM Prijava");
 	}
 }
